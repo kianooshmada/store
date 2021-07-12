57c379f7a666ceb76446850fe5cf51e24b7f1584
kianooshmada <kianooshnike1365@gmail.com>
2021-07-12 15:26:35 +0430

    fifth

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default class Product extends Component {
   render() {
+    const { id, img } = this.props.product || {};
+
+    console.log("p:" + id);
     return (
-      <div>
-        <h3>hello from Product</h3>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("you clicked me on the image container")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
