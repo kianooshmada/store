fc5514493b410d01c65aad4d2f91832508ec03bb
kianooshmada <kianooshnike1365@gmail.com>
2021-07-14 12:20:08 +0430

    eighth

--- src/context.js
+++ src/context.js
@@ -23,8 +23,16 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("hello from deatils");
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id);
+    return product;
+  };
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
 
   addToCart = (id) => {
